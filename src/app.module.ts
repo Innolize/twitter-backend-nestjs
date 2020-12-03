21ddc493db48ed6e585e19188dde6b4b5e114dea
Roman <Roman.Mettler@gmail.com>
2020-12-03 10:00:03 -0300

    Crea Roles de access control

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,18 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose'
+import { AccessControlModule } from 'nest-access-control';
 
 import { PostModule } from './post/post.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { roles } from './app.roles';
 
 
 @Module({
 
   imports: [
     MongooseModule.forRoot('mongodb://localhost/twitter-database', { useFindAndModify: false }),
+    AccessControlModule.forRoles(roles),
     PostModule,
     UserModule,
     AuthModule],
